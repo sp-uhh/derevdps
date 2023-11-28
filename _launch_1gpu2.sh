da0407e621f16d615dd4a0cbc4226997e910bad1
lemercier <lemercier@informatik.uni-hamburg.de>
2023-11-28 16:50:34 +0000

    Branching out to test with Eloi's target preconditioning

--- _launch_1gpu2.sh
+++ _launch_1gpu2.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=8          # We have 64 total in spgpu2 and 32 in spgpu1, making it 8 cores per GPU process in both cases
 #SBATCH --partition=all
 #SBATCH --nodelist=spgpu2          # Or set it to spgpu1
-#SBATCH --job-name=song_vctk_1_170
+#SBATCH --job-name=song_vctk_1_150
 #SBATCH --output=.slurm/%x-%j.out    # Save to folder ./jobs, %x means the job name. You may need to create this folder
 #SBATCH --error=.slurm/%x-%j.err
 #SBATCH --time=4-00:00             # Limit job to 4 days
@@ -56,7 +56,7 @@ srun -K1 -u python3 train.py \
     --base_dir $base_dir \
     --testset_dir $data_dir/vctk_derev_with_rir \
     --batch_size 16 \
-    --gpus 1 \
+    --gpus 3, \
     --spec_abs_exponent 1. \
     --spec_factor 1 \
     --condition none \
