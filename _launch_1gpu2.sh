bc71eb9313aa1a8b479dffce536dea4c22c9c503
lemercier <lemercier@informatik.uni-hamburg.de>
2023-11-28 17:40:04 +0000

    s

--- _launch_1gpu2.sh
+++ _launch_1gpu2.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=8          # We have 64 total in spgpu2 and 32 in spgpu1, making it 8 cores per GPU process in both cases
 #SBATCH --partition=all
 #SBATCH --nodelist=spgpu2          # Or set it to spgpu1
-#SBATCH --job-name=song_vctk_1_150
+#SBATCH --job-name=edm_vctk_1_150_pt
 #SBATCH --output=.slurm/%x-%j.out    # Save to folder ./jobs, %x means the job name. You may need to create this folder
 #SBATCH --error=.slurm/%x-%j.err
 #SBATCH --time=4-00:00             # Limit job to 4 days
@@ -68,7 +68,7 @@ fi;
 #     --sigma_max 150
 
 # VCTK EDM Scale Factor = 1 (comparison Eloi)
-base_dir="$data_dir/vctk_56spk/audio"
+base_dir=$data_dir/vctk_56spk/audio
 format="vctk"
 srun -K1 -u python3 train.py \
     --backbone ncsnpp \
@@ -87,3 +87,27 @@ srun -K1 -u python3 train.py \
     --sigma_min 1e-5 \
     --sigma_max 150 \
     --sigma_data 1.7
+
+
+
+
+
+# python3 train.py \
+#     --backbone ncsnpp \
+#     --format  $format \
+#     --base_dir $base_dir \
+#     --testset_dir $data_dir/vctk_derev_with_rir \
+#     --batch_size 2 \
+#     --gpus 2, \
+#     --spec_abs_exponent 1. \
+#     --spec_factor 1 \
+#     --condition none \
+#     --sde edm \
+#     --preconditioning karras \
+#     --num_eval_files 10 \
+#     --num_unconditional_files 25 \
+#     --sigma_min 1e-5 \
+#     --sigma_max 150 \
+#     --sigma_data 1.7 \
+#     --limit_train_batches 25 \
+#     --limit_val_batches 10
